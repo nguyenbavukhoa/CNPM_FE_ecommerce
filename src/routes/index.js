9295eb52a874144a56675ddf6bfe077efbaf6ac9
nguyenbavukhoa <42162681+nguyenbavukhoa@users.noreply.github.com>
2025-11-13 00:19:08 +0700

    feat(checkout): Xây dựng UI cơ bản trang checkout (WIP) [Refs #8] Phát triển giao diện (UI) cơ bản cho trang checkout, bao gồm: * Form nhập thông tin khách hàng. * Tóm tắt giỏ hàng (order summary).
    Phần tích hợp cổng thanh toán và xử lý logic đặt hàng CHƯA hoàn thành (mới đạt ~70% yêu cầu của issue). Sẽ tiếp tục ở commit sau.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,31 @@
 import { useSearchParams } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
-import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
+//import OrderHistoryPage from "../pages/OrderHistoryPage/OrderPage.jsx";
 import AuthPage from "../pages/AuthPage/AuthPage.jsx";
+// import AdminPage from "../pages/AdminPage/AdminPage";
 // import AdminLogin from "../pages/AdminLogin.jsx/AdminLogin.jsx";
-// import CheckoutPage from "../pages/CheckoutPage/CheckoutPage"; // Import trang Checkout
-
+import CheckoutPage from "../pages/CheckoutPage/CheckoutPage"; // Import trang Checkout
+//import ProfilePage from "../pages/ProfilePage/ProfilePage";
 export const routes = [
   {
     path: "/",
     page: HomePage,
     isShowHeader: true,
   },
+  {
+    path: "/checkout",
+    page: CheckoutPage,
+    // isShowHeader: true,
+  },
   // {
-  //   path: "/checkout",
-  //   page: CheckoutPage,
-  //   // isShowHeader: true,
+  //   path: "/order-history",
+  //   page: OrderHistoryPage,
+  //   isShowHeader: true,
+  // },
+  // {
+  //   path: "/user-info", // For signup and signin
+  //   page: ProfilePage,
+  //   isShowHeader: true,
   // },
   {
     path: "/auth", // For signup and signin
@@ -22,13 +33,13 @@ export const routes = [
     isShowHeader: true,
   },
   // {
+  //   path: "/admin", // For admin dashboard
+  //   page: AdminPage,
+  //   // isShowHeader: true,
+  // },
+  // {
   //   path: "/admin-login", // For signin admin
   //   page: AdminLogin,
   //   // isShowHeader: true,
   // },
-
-  {
-    path: "*",
-    page: NotFoundPage,
-  },
 ];
